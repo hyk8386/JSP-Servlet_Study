f5f1941157db493a2faa504cfddffc6efa63651a
DESKTOP-GOC4BUG\505 <hyk8386@naver.com>
2022-07-01 18:16:25 +0900

    chap09.데이터베이스를 이용한 회원 관리 시스템 구축하기

---
+++ Book/chap09/src/main/java/com/saeyan/dao/MemberDAO.java
@@ -0,0 +1,196 @@
+package com.saeyan.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import com.saeyan.dto.MemberVO;
+
+public class MemberDAO {
+	private MemberDAO() {
+		
+	}
+	
+	private static MemberDAO instance = new MemberDAO();
+	
+	public static MemberDAO getInstance() {
+		return instance;
+	}
+
+	
+	
+	public Connection getConnection() throws Exception {
+		
+		Connection conn = null;
+		Context initContext = new InitialContext();
+		Context envContext = (Context)initContext.lookup("java:/comp/env");
+		DataSource ds = (DataSource)envContext.lookup("jdbc/oracle");
+		conn = ds.getConnection();
+		return conn;
+	}
+	
+	
+	
+	// 사용자 인증시 사용하는 메소드
+	public int userCheck(String userid, String pwd) {
+		int result = -1;
+		String sql = "select pwd from member where userid=?";   // ?에 아이디를 입력하면 패스워드 하나만 검색됨
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userid);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				if(rs.getString("pwd") !=null && rs.getString("pwd").equals(pwd)) {  // 패스워드가 공백이 아니고, 가져온 암호와 같은지?
+					result = 1; // 패스워드 정상
+				} else {
+					result = 0; // 패스워드 오류
+				} 
+			} else {
+				result = -1;  // 레코드가 없는 경우 (데이터베이스 자체가 안된거?)
+			}
+		} catch (Exception e) {
+			e.printStackTrace();  // 데이터베이스에 잘못된 값을 넣었을경우?
+		} finally {
+			try {
+				if(rs != null) rs.close();
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+	
+	// 아이디로 회원 정보 가져오는 메소드
+	public MemberVO getMember(String userid) {
+		MemberVO mVo = null;
+		String sql = "select * from member where userid=?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userid);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				mVo = new MemberVO();
+				mVo.setName(rs.getString("name"));
+				mVo.setUserid(rs.getString("userid"));
+				mVo.setPwd(rs.getString("pwd"));
+				mVo.setEmail(rs.getString("email"));
+				mVo.setPhone(rs.getString("phone"));
+				mVo.setAdmin(rs.getInt("admin"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs != null) rs.close();
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return mVo;
+	}
+
+
+	public int confirmID(String userid) {
+		int result = -1;
+		String sql = "select pwd from member where userid=?";   
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userid);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				result = 1;
+			} else {
+				result = -1;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs != null) rs.close();
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+	
+	
+	public int insertMember(MemberVO mVo) {
+		int result = -1;
+		String sql = "insert into member values(?, ?, ?, ?, ?, ?)";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, mVo.getName());
+			pstmt.setString(2, mVo.getUserid());
+			pstmt.setString(3, mVo.getPwd());
+			pstmt.setString(4, mVo.getEmail());
+			pstmt.setString(5, mVo.getPhone());
+			pstmt.setInt(6, mVo.getAdmin());
+			result = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+	
+	public int updateMember(MemberVO mVo) {
+		int result = -1;
+		String sql = "update member set pwd=?, email=?," + "phone=?, admin=? where userid=?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, mVo.getPwd());
+			pstmt.setString(2, mVo.getEmail());
+			pstmt.setString(3, mVo.getPhone());
+			pstmt.setInt(4, mVo.getAdmin());
+			pstmt.setString(5, mVo.getUserid());
+			result = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+		
+	}
+}
+	
